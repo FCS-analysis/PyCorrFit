Fixed issue #17 (Human readable units). Improved command-line handling of session files.

src/models/__init__.py
@@ -93,11 +93,12 @@ def AppendNewModel(Modelarray):
             verification[texts[0]] = verify
             
 def GetHumanReadableParms(model, parameters):
-    """ From a set of parameters that have some strange units e.g. [100 nm],
+    """ From a set of parameters that have internal units e.g. [100 nm],
         Calculate the parameters in human readable units e.g. [nm].
         Uses modeldict from this module.
         *model* - an integer ID of a model
-        *parameters* - a list/array of parameters (all parameters of that model)
+        *parameters* - a list/array of parameters 
+                       (all parameters of that model)
         Returns:
         New Units, New Parameters
     """
@@ -117,11 +118,15 @@ def GetHumanReadableParms(model, parameters):
 
 
 def GetHumanReadableParameterDict(model, names, parameters):
-    """ From a set of parameters that have some strange units e.g. [100 nm],
+    """ From a set of parameters that have internal units e.g. [100 nm],
         Calculate the parameters in human readable units e.g. [nm].
         Uses modeldict from this module.
+        In contrast to *GetHumanReadableParms* this function accepts
+        single parameter names and does not need the full array of
+        parameters.
         *model* - an integer ID of a model
-        *name of parameters* - order should be same as in
+        *name* - the names of the parameters to be translated,
+                 order should be same as in
         *parameters* - a list of parameters
         Returns:
         New Units, New Parameters
@@ -154,6 +159,29 @@ def GetHumanReadableParameterDict(model, names, parameters):
         # readable.
         return names, parameters
 
+
+def GetInternalFromHumanReadableParm(model, parameters):
+    """ This is the inverse of *GetHumanReadableParms*
+        *model* - an integer ID of a model
+        *parameters* - a list/array of parameters 
+        Returns:
+        New Units, New Parameters
+    """
+    stdparms = valuedict[model]
+    if len(stdparms) == 5:
+        # This means we have extra information on the model
+        # and can convert to internal values
+        OldParameters = 1.*np.array(parameters)
+        Facors = 1./np.array(stdparms[4])        # inverse
+        NewParameters = 1.*OldParameters*Facors
+        NewUnits = stdparms[0]
+        return NewUnits, NewParameters
+    else:
+        # There is no info about human readable stuff. The given 
+        # parameters have not been converted befor using
+        # *GetHumanReadableParms*.
+        return stdparms[0], parameters
+        
 
 def GetMoreInfo(modelid, Page):
     """ This functino is called by someone who has already calculated

src/page.py
@@ -82,6 +82,12 @@ class FittingPanel(wx.Panel):
         self.counter = counter
         # Model we are using
         self.modelid = modelid
+        # modelpack:
+        # [0] labels
+        # [1] values
+        # [2] bool values to fit
+        # [3] labels human readable (optional)
+        # [4] factors human readable (optional)
         modelpack = mdls.modeldict[modelid]
         # The string of the model in the menu
         self.model = modelpack[1]
@@ -92,12 +98,10 @@ class FittingPanel(wx.Panel):
         # Parameter verification function.
         # This checks parameters concerning their physical meaningfullness :)
         self.check_parms = mdls.verification[modelid]
-        # Active Parameters we are using for the fitting
+        # active_parameters:
         # [0] labels
         # [1] values
         # [2] bool values to fit
-        # [3] labels human readable (optional)
-        # [4] factors human readable (optional)
         self.active_parms = active_parms
         # Some timescale
         self.taufull = tau
@@ -159,25 +163,43 @@ class FittingPanel(wx.Panel):
             pages parameters.
             This function is called when the "Apply" button is hit.
         """
+        parameters = list()
         # Read parameters from form and update self.active_parms[1]
         for i in np.arange(len(self.active_parms[1])):
-            self.active_parms[1][i] = self.spincontrol[i].GetValue()
+            parameters.append(1*self.spincontrol[i].GetValue())
             self.active_parms[2][i] = self.checkboxes[i].GetValue()
+        # As of version 0.7.5: we want the units to be displayed
+        # human readable - the way they are displayed 
+        # in the Page info tool.
+        # Here: Convert human readable units to program internal
+        # units
+        e, self.active_parms[1] = mdls.GetInternalFromHumanReadableParm(
+                                  self.modelid, np.array(parameters))
         self.active_parms[1] = self.check_parms(1*self.active_parms[1])
         # If parameters have been changed because of the check_parms
         # function, write them back.
-        for i in np.arange(len(self.active_parms[1])):
-            self.spincontrol[i].SetValue(self.active_parms[1][i])
+        self.apply_parameters_reverse()
 
 
     def apply_parameters_reverse(self, event=None):
         """ Read the values from the pages parameters and write
             it to the form.
         """
-        # Write parameters to the form on the Page
+        # check parameters
         self.active_parms[1] = self.check_parms(self.active_parms[1])
+        #
+        # As of version 0.7.5: we want the units to be displayed
+        # human readable - the way they are displayed 
+        # in the Page info tool.
+        # 
+        # Here: Convert program internal units to
+        # human readable units
+        labels, parameters = \
+                     mdls.GetHumanReadableParms(self.modelid,
+                                        self.active_parms[1])
+        # Write parameters to the form on the Page
         for i in np.arange(len(self.active_parms[1])):
-            self.spincontrol[i].SetValue(self.active_parms[1][i])
+            self.spincontrol[i].SetValue(parameters[i])
             self.checkboxes[i].SetValue(self.active_parms[2][i])
 
 
@@ -339,7 +361,8 @@ class FittingPanel(wx.Panel):
         self.chi2 = Fitting.chi
         for i in np.arange(len(parms)):
             self.active_parms[1][i] = parms[i]
-            self.spincontrol[i].SetValue(parms[i])
+        # Update spin-control values
+        self.apply_parameters_reverse()
         # Plot everthing
         self.PlotAll()
         # Return cursor to normal
@@ -382,7 +405,14 @@ class FittingPanel(wx.Panel):
         sizer = wx.StaticBoxSizer(box, wx.VERTICAL)
         check = list()
         spin = list()
-        for label in self.active_parms[0]:
+        #
+        # As of version 0.7.5: we want the units to be displayed
+        # human readable - the way they are displayed 
+        # in the Page info tool.
+        # 
+        labels, parameters = mdls.GetHumanReadableParms(self.modelid,
+                                                self.active_parms[1])
+        for label in labels:
             sizerh = wx.BoxSizer(wx.HORIZONTAL)
             checkbox = wx.CheckBox(self.panelsettings, label=label)
             # We needed to "from wx.lib.agw import floatspin" to get this:
@@ -552,9 +582,13 @@ class FittingPanel(wx.Panel):
         # Make the check boxes and spin-controls available everywhere
         self.checkboxes = check
         self.spincontrol = spin
-        # Show what is inside active_parms
-        labels = self.active_parms[0]
-        parameters = self.active_parms[1]
+        #
+        # As of version 0.7.5: we want the units to be displayed
+        # human readable - the way they are displayed 
+        # in the Page info tool.
+        # 
+        labels, parameters = mdls.GetHumanReadableParms(self.modelid,
+                                                self.active_parms[1])
         parameterstofit = self.active_parms[2]
         # Set initial values given by user/programmer for Diffusion Model
         for i in np.arange(len(labels)):

src/tools/simulation.py
@@ -20,6 +20,7 @@ import wx
 import numpy as np
 
 import edclasses  # edited floatspin
+import models as mdls
 
 
 class Slide(wx.Frame):
@@ -306,9 +307,20 @@ class Slide(wx.Frame):
         # And Plot
         idA = self.droppA.GetSelection()
         idB = self.droppB.GetSelection()
-        self.Page.active_parms[1][idA] = self.valueA
-        self.Page.active_parms[1][idB] = self.valueB
-
+        # As of version 0.7.5: we want the units to be displayed
+        # human readable - the way they are displayed 
+        # in the Page info tool.
+        # Convert from human readable to internal units
+        # The easiest way is to make a copy of all parameters and
+        # only write back those that have been changed:
+        # 
+        parms_0 = 1.*np.array(mdls.valuedict[self.modelid][1])
+        parms_0[idA] = self.valueA # human readable units
+        parms_0[idB] = self.valueB # human readable units
+        label, parms_i =\
+            mdls.GetInternalFromHumanReadableParm(self.modelid, parms_0)
+        self.Page.active_parms[1][idA] = parms_i[idA]
+        self.Page.active_parms[1][idB] = parms_i[idB]
         self.Page.apply_parameters_reverse()
         self.Page.PlotAll()
 
@@ -316,8 +328,13 @@ class Slide(wx.Frame):
     def SetStart(self):
         # Sets first and second variable of a page to
         # Parameters A and B respectively.
-        self.parmAlist = self.Page.active_parms[0]
-        self.parmBlist = self.Page.active_parms[0]
+        self.modelid = self.Page.modelid
+        ParmLabels, ParmValues = \
+                   mdls.GetHumanReadableParms(self.modelid,
+                                              self.Page.active_parms[1])
+
+        self.parmAlist = ParmLabels
+        self.parmBlist = ParmLabels
         # Operators
         # Calculation of variable A with fixed B
         self.opdict = dict()
@@ -328,19 +345,29 @@ class Slide(wx.Frame):
         self.labelB = self.parmBlist[1]
         self.labelOp = self.oplist[0]
         self.opfunc = self.opdict[self.labelOp]
-        self.valueA = self.Page.active_parms[1][0]
-        self.valueB = self.Page.active_parms[1][1]
-        self.valueB, self.valueOp = self.CalcFct(self.valueA, self.valueB, 0)
+        self.valueA = ParmValues[0]
+        self.valueB = ParmValues[1]
+        self.valueB, self.valueOp = self.CalcFct(self.valueA, 
+                                                         self.valueB, 0)
 
 
     def SetValues(self, event=None):
         # Set the values for spin and slider
+        # As of version 0.7.5: we want the units to be displayed
+        # human readable - the way they are displayed 
+        # in the Page info tool.
+        #
         # Parameter A
         idA = self.droppA.GetSelection()
-        self.valueA = self.Page.active_parms[1][idA]
         # Parameter B
         idB = self.droppB.GetSelection()
-        self.valueB = self.Page.active_parms[1][idB]
+        # self.valueB = self.Page.active_parms[1][idB]
+        # self.valueA = self.Page.active_parms[1][idA]
+        ParmLabels, ParmValues = \
+                   mdls.GetHumanReadableParms(self.modelid,
+                                              self.Page.active_parms[1])
+        self.valueA = ParmValues[idA]
+        self.valueB = ParmValues[idB]                             
         # Operator
         idop = self.dropop.GetSelection()
         keys = self.opdict.keys()