ecf8ab02b98ecc52862255f70f983be632edbb3c
paulmueller <paul.mueller@biotec.tu-dresden.de>
2013-05-22 16:26:15 +0200

    Update windowsXP_32bit_bundle_script.bat

--- pyinstaller-howto/windowsXP_32bit_bundle_script.bat
+++ pyinstaller-howto/windowsXP_32bit_bundle_script.bat
@@ -1,4 +1,4 @@
-cd C:\Python27\PyCorrFit\pyinstaller
+cd C:\Python27\pyinstaller
 del /q /s ..\PyCorrFit\pyinstaller-howto\build
 del /q /s ..\PyCorrFit\pyinstaller-howto\dist
 python pyinstaller.py ..\PyCorrFit\pyinstaller-howto\PyCorrFit_win.spec
