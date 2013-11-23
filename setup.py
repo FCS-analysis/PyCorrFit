589c02a7059ef2720fba5963aff6e5c03065ff72
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2013-11-23 14:52:02 +0100

    - Edited setup.py to automatically get the version from ChangeLog.txt. - Module import now has correct version number.
    Former-commit-id: 8a630027a4b11d4f8c6e7e411a450e9a9cce4baf [formerly ba60aa4176c02df3665a716677a22d4e927b61b9] [formerly a8758038f3fd0fa29516b10cb3e0f28ed29d612e] [formerly 9e08baea77f292693a6c3d12584546326761bfad [formerly 88689d092d347ac7a4e0c130262367a262af048c] [formerly a8758038f3fd0fa29516b10cb3e0f28ed29d612e [formerly 75f57fad3dd0cc722ae366b41d7c26ef13b60051]]]
    Former-commit-id: 37f80f01e1efe2e33d94adb7a18e927e0e254286 [formerly 82a0da080f77981982a189102318823a0d7abd6f] [formerly bb3518146c9c9fecc299ebd77824e558dd4138db [formerly 7edbfb4cc590f92ac530dd2051648b849edfebe9]]
    Former-commit-id: bb3518146c9c9fecc299ebd77824e558dd4138db
    Former-commit-id: 38dfb285320a3a9a2fa25213e4f4b2fd9f5ad89a

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-from os.path import join, dirname
+from os.path import join, dirname, realpath
+
+
+# Get the version of PyCorrFit from the Changelog.txt
+StaticChangeLog = join(dirname(realpath(__file__)), "ChangeLog.txt")
+try:
+    clfile = open(StaticChangeLog, 'r')
+    version = clfile.readline().strip()
+    clfile.close()     
+except:
+    version = "0.0.0-unknown"
 
 setup(
     name='pycorrfit',
     author='Paul Mueller',
     author_email='paul.mueller@biotec.tu-dresden.de',
     url='https://github.com/paulmueller/PyCorrFit',
-    version='0.8.0',
+    version=version,
     packages=['pycorrfit','pycorrfit.models','pycorrfit.readfiles','pycorrfit.tools'],
     package_dir={'pycorrfit': 'src','pycorrfit.models': 'src/models','pycorrfit.readfiles': 'src/readfiles','pycorrfit.tools': 'src/tools'},
     long_description=open(join(dirname(__file__), 'README.md')).read(),
