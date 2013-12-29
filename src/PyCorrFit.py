2e2687cb9ca2231ad42018e4687192c849885887
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2013-12-29 12:08:52 +0100

    gpl texts

--- src/PyCorrFit.py
+++ src/PyCorrFit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """ PyCorrFit
-    Paul Müller, Biotec - TU Dresden
 
     A flexible tool for fitting and analyzing correlation curves.
 
@@ -13,16 +12,17 @@
     unit of inverse area: 100 /um^2
     unit of inv. volume : 1000 /um^3
 
+    Copyright (C) 2011-2012  Paul Müller
 
-    PyCorrFit is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published 
-    by the Free Software Foundation, either version 2 of the License, 
-    or (at your option) any later version.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-    PyCorrFit is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of 
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-    See the GNU General Public License for more details. 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License 
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
