ca6e57a58bffc462532efce3a09d7f69fc878048
paulmueller <paul.mueller@biotec.tu-dresden.de>
2014-03-06 21:14:07 +0100

    Added bundle script for Ubuntu 13.10
    Former-commit-id: 0b4c952b8946be101c4e9a364550869f9c7b0402 [formerly 86dcf051b5a3bf2c0a51ec3951c553ad544bedc9] [formerly 3e47a12480240e3b907aecdfb236011ca2964b0c [formerly b79cd500b4921450f1e3812b1f954c41483b6cc2]] [formerly b35fcc7ef6669523606bb271c2dc6bebd38d78e0 [formerly 21b0a109bfb869fdbad2c1417d61edb5a8b0aaa9] [formerly 3bd160134333995330b885febbdad478748310ed [formerly 1a397513f78e8293fe8a5359b11cff7ad89d0348]]]
    Former-commit-id: 9483d1b3ace11654e7c63eaedc8cde37b3c8f021 [formerly c7e325ca4c4dfce0960646048411ebde0c7213d8] [formerly b33d26e19f0ee1c698b2e988a34233e1289ffee3 [formerly 278ff25fb66d487a7db44cbace3b7b163cf94865]]
    Former-commit-id: e46398aa17be0c501377c145e4d8f27c983db4e6 [formerly cdee467dd240d384c7fc315527b80f9cd8fd99fa]
    Former-commit-id: 54dba966016d1b1c81c8e51b9078d60c4e918a5c

---
+++ pyinstaller-howto/ubuntu13.10_bundle_script.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# Works with 
+# pyinstaller-develop fdeef345233bc13836f2b4bf6fa15ac55b8563ac
+# **************** Change Variables Here ************
+# Directory structure:
+# ./PyCorrFit                     # git repository
+PyInstaller="pyinstaller-fdeef345233bc13836f2b4bf6fa15ac55b8563ac/"
+# Progname.py should be in the Progdir
+Progname="PyCorrFit"
+# We require a ChangeLog.txt and a source directory in the Progdir
+# BASEDIR/PyCorrFit/pyinstaller-howto
+BASEDIR=$(dirname $BASH_SOURCE)
+cd $BASEDIR
+BASEDIR=$(pwd)
+cd "../../"
+StartDir=$(pwd)"/"
+Progdir=${StartDir}${Progname}"/"
+# Virtualenv
+PyInstallerDir=${StartDir}${Progname}"/"${PyInstaller}
+Specfile=${BASEDIR}"/"${Progname}"_linux.spec"
+echo $Specfile
+
+echo "***************************************"
+echo "* Creating "${Progname}" binary       *"
+echo "***************************************"
+
+
+# These lines were used to create a source zip file
+# But we have a setup.py file now and don't need it anymore.
+# We require a Progname_doc.tex in the source-doc directory
+#DocDir=${StartDir}${Progname}"/doc-src/"
+#BinDir=${StartDir}${Progname}"/"
+#WD="/tmp/"${Progname}"/"
+#mkdir -p $WD
+#
+#cp -R ${StartDir}${Progname}/src/* $WD
+#cp -R ${StartDir}${Progname}/ChangeLog.txt $WD
+#
+#echo " *** Removing *.pyc files! *** "
+#find  $WD -iname "*.pyc" -exec rm -r -f {} \;
+#
+#echo " *** Removing *.py~ files! *** "
+#find  $WD -iname "*.py~" -exec rm -r -f {} \;
+#
+#echo " *** Adding documentation *** "
+#cd $DocDir
+#pdflatex -synctex=1 -interaction=nonstopmode ${Progname}_doc.tex >> /dev/null
+#bibtex ${Progname}_doc.aux >> /dev/null
+#bibtex ${Progname}_doc.aux >> /dev/null
+#pdflatex -synctex=1 -interaction=nonstopmode ${Progname}_doc.tex >> /dev/null
+#pdflatex -synctex=1 -interaction=nonstopmode ${Progname}_doc.tex >> /dev/null
+#
+#mkdir -p ${WD}/doc
+#cp ${Progname}_doc.pdf ${WD}/doc/
+#
+#echo " *** Creating src zip file! *** "
+#
+#cd ${Progdir}
+#zipname=${Progname}"_"
+#zipname+=$(head -n1 ./ChangeLog.txt | tr -d "\r\n")
+#zipname+="_src.zip"
+#
+#cd $WD"../"
+#
+#zip -r ${BinDir}${zipname} ${Progname} >> /dev/null
+
+#rm -R $WD
+
+cd $Progdir
+
+if [ -f $Specfile ]
+then
+    # added following line (remove build directory beforehand!)
+    # a.datas += [('doc/ChangeLog.txt', '/PATH/TO/PyCorrFit/ChangeLog.txt', 'DATA')]
+    python ${PyInstallerDir}pyinstaller.py -F $Specfile
+else
+    python ${PyInstallerDir}pyinstaller.py -F ${Progdir}"src/"${Progname}".py"
+fi
+cd ${Progdir}
+bname=${Progname}"_"
+bname+=$(head -n1 ./ChangeLog.txt | tr -d "\r\n")
+bname+="_Ubuntu13-10"
+
+zname="Ubuntu13-10_"
+zname+=${Progname}"_"
+zname+=$(head -n1 ./ChangeLog.txt | tr -d "\r\n")
+zname+=".zip"
+
+
+#name+=$(uname -r)
+
+
+mv ${Progdir}"dist/"${Progname} ${Progdir}"dist/"${bname}
+# Cleanup
+#rm -R ${Progdir}"/build"
+
+# Zip the release
+zip -j ${Progdir}"dist/"${zname} ${Progdir}"dist/"${bname}
+
