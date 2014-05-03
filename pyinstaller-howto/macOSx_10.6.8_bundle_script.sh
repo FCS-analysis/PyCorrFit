0ba942626172fd1fbfc26d8ed0d81b78fd97229b
Paul Mueller <paul.mueller@biotec.tu-dresden.de>
2014-05-03 20:30:49 +0200

    Managed to create first .app bundle

--- pyinstaller-howto/macOSx_10.6.8_bundle_script.sh
+++ pyinstaller-howto/macOSx_10.6.8_bundle_script.sh
@@ -9,6 +9,12 @@ cd "../"
 # working with stable 2.0 release (MD5 19350c07632e4deef5f4ecf64a556637)
 # and manual python 2.7 installation.
 
-python pyinstaller-2.0/pyinstaller.py -F pyinstaller-howto/PyCorrFit_mac.spec
+python ./Pyinstaller-2.1/pyinstaller.py -y ./pyinstaller-howto/PyCorrFit_mac.spec
 
-#python pyinstaller-develop/pyinstaller.py -F pyinstaller-howto/PyCorrFit_mac.spec
+# move aside the binary and replace with script
+
+mv ./dist/PyCorrFit.app/Contents/MacOS/PyCorrFit ./dist/PyCorrFit.app/Contents/MacOS/PyCorrFit.bin
+
+cp ./pyinstaller-howto/macOSx_script_starter.sh ./dist/PyCorrFit.app/Contents/MacOS/PyCorrFit
+
+chmod +x ./dist/PyCorrFit.app/Contents/MacOS/PyCorrFit
