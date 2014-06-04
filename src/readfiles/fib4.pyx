9f073e6665c835fd80b1d58cf25b7fe5aaa160cd
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2014-06-04 22:20:52 +0200

    Started implementation of PicQuant data file format (Issue #69) - decided to write a wrapper for crucial functions from FCS_viewer - firstly, auto- and cross-correlation will be supported. PIE might follow.
    Former-commit-id: 2846319bb5d82b9038f0db3a03b54666e1f90aea [formerly 5f386c25cf126fe3d69bd2f2e3a038d7b53efa05] [formerly e7330532ddc63bd636f0e4f488330826cb75cdbe [formerly b597b8108ebf656466c0f5ae6d8c82be21950f57]] [formerly 85f209f1531dd781477f77839525f4c306dec6fb [formerly 3b00de9e57aed23342942b684da1d21e6f1520b4] [formerly ddbbf5d264447ddfe0f81a330ad7bcad8d43ac97 [formerly a6beae21e7b67307ab4a4886e805794611a75dfe]]]
    Former-commit-id: 76990fd0abab78ef73ac13dd2f2a19d7c3fff479 [formerly 8ff314a43058fb5e965242b8ed9e3d9ca22edb17] [formerly 9dba93c0bfd87bf2681a02abbd1240f795f6fdb9 [formerly 214febc890638177fcb957b0d5631ba16de9d814]]
    Former-commit-id: b76af2948ba412099dc725e94727cf50077c6244 [formerly 14e6e1d02e53bde60c17475053ae68d040da6e7d]
    Former-commit-id: 4d3c2d591c88788d56ce5b351e08fab04f5bb7d9

---
+++ src/readfiles/fib4.pyx
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+""" PicoQuant functionalities from FCS_viewer
+
+    This file contains a fast implementation of an algorithm that is
+    very important (yes I have no clou about the inside of pt3 files)
+    for importing *.pt3 files `dividAndConquer`.
+    
+    The code was written by
+    Dr. Dominic Waithe
+    Wolfson Imaging Centre.
+    Weatherall Institute of Molecular Medicine.
+    University of Oxford
+
+    https://github.com/dwaithe/FCS_viewer
+
+    See Also:
+        The wrapper: `read_pt3_PicoQuant.py`
+        The wrapped file: `read_pt3_PicoQuant_original_FCSViewer.py`.
+
+"""
+
+import cython
+cimport cython
+
+import numpy as np
+cimport numpy as np
+
+DTYPE = np.float64
+ctypedef np.float64_t DTYPE_t
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+def dividAndConquer(arr1b,arr2b,arrLength):
+    """divide and conquer fast intersection algorithm. Waithe D 2014"""
+    
+    cdef np.ndarray[DTYPE_t, ndim=1] arr1bool = np.zeros((arrLength-1))
+    cdef np.ndarray[DTYPE_t, ndim=1] arr2bool = np.zeros((arrLength-1))
+    cdef np.ndarray[DTYPE_t, ndim=1] arr1 = arr1b
+    cdef np.ndarray[DTYPE_t, ndim=1] arr2 = arr2b
+    
+    cdef int arrLen 
+    arrLen = arrLength;
+    cdef int i
+    i = 0;
+    cdef int j 
+    j = 0;
+   
+    while(i <arrLen-1 and j< arrLen-1):
+       
+        if(arr1[i] < arr2[j]):
+          i+=1;
+        elif(arr2[j] < arr1[i]):
+          j+=1;
+        elif (arr1[i] == arr2[j]):
+        
+          arr1bool[i] = 1;
+          arr2bool[j] = 1;
+          i+=1;
+    
+    return arr1bool,arr2bool
