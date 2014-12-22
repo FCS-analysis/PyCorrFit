371f0f9a820eae39d52b13112533c308e4f5a8ea
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2014-12-22 22:45:22 +0100

    main method
    Former-commit-id: 6924c6dedd275736a4275b3bae96f256ec6860b7 [formerly 4221ab486c78808c4011fad440b0a4ea759ac8c4] [formerly 4f4f664dcc37034f9033baba578216b83acc87b0 [formerly 0f7e0092088a8c05e7932fb98f605fbedfff7b7c]] [formerly 708ca0d73ddfa113c55a4dbb7283708e0a395a3c [formerly 2672a41735686bc205a81d2be17652220eaa8ab9] [formerly 437096b538027aec2256224ffb4145f6fd464831 [formerly d3eeb884dd81943ee5887beafbbd4af086625260]]]
    Former-commit-id: 76fcd38c602f305e69be05593afda8bce00da28e [formerly 89ac841af2b463825bd59e6b64c64a2701173726] [formerly 44716503a3b9b216a6082ba193f13778b0839d0f [formerly e2ecee1b00d79c16620e1dedf5881803e0777aa5]]
    Former-commit-id: d9c36214ca90d6626310e69395dd5367c33490e5 [formerly dc19fa918a6598f271be709d3be46176bdce129a]
    Former-commit-id: 77acfbf855c954609e30c46e13b09cd6e79778a9

---
+++ pycorrfit/__main__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    When a membrane is scanned perpendicularly to its surface, the
+    fluorescence signal originating from the membrane itself must be
+    separated from the signal of the surrounding medium for an FCS
+    analysis. PyCorrFit interactively extracts the fluctuating
+    fluorescence signal from such measurements and applies a
+    multiple-tau algorithm. The obtained correlation curves can be
+    evaluated using PyCorrFit.
+
+    Copyright (C) 2011-2012  Paul Müller
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License 
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from . import doc
+from . import PyCorrFit
+
+## VERSION
+version = doc.__version__
+__version__ = version
+
+PyCorrFit.Main()
