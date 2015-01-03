93254426263de38ddf4ef406fa0d18d52a89d195
paulmueller <paul.mueller@biotec.tu-dresden.de>
2015-01-03 20:59:47 +0100

    remove old winxp build files
    Former-commit-id: 324b07e3d65a2eafb0919dbb286700ff1a6ddc45 [formerly 7787c2fda848b8d84dd74db951ace655c9a607d0] [formerly e5b6c28a3fa22c2ba06cdc8bd824ad9a716d3e3c [formerly d3d03416b616ec0a7b90792d6dfeac7629c45d84]] [formerly a927a71a645bbd44517aa15b5859333095071338 [formerly 25f23d0cc41fed0d4022d17dd693285a085bb38f] [formerly ddb1dabbba44eba8f29690783b45ac7605c0989f [formerly 3b9b2d1ba95464d92eeedf1ab6e92fe2a508f9f4]]]
    Former-commit-id: 73991fadb54f541c353b460db50b118e18a368e1 [formerly 2bc64afe0143c7d03b6ba5dc135423d84d527aad] [formerly 55918a9236ab17c6ac49427e80d33095202e71ef [formerly 2cf9a6adc928f7c210830d6156d368c52320a383]]
    Former-commit-id: 04458db9e922047729a045f3166579776c2866ae [formerly fc94d02e33c367f81a7bee8d83d8545b0210a00c]
    Former-commit-id: dfded5386150aae384a423a94c5ee2c52ab907ef

--- freeze_pyinstaller/windowsXP_32bit_bundle_script.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Python27\pyinstaller
-del /q /s ..\PyCorrFit\freeze_pyinstaller\build
-del /q /s ..\PyCorrFit\freeze_pyinstaller\dist
-python pyinstaller.py ..\PyCorrFit\freeze_pyinstaller\PyCorrFit_win.spec
