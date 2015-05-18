8c632c577befa4c30d405ca6effe596e20c5b561
paulmueller <paul.mueller@biotec.tu-dresden.de>
2015-05-18 08:58:55 +0200

    move license to separate file

--- pycorrfit/frontend.py
+++ pycorrfit/frontend.py
@@ -1,31 +1,9 @@
 # -*- coding: utf-8 -*-
-u""" PyCorrFit - Module "frontend"
+"""
+PyCorrFit - Module "frontend"
 
 The frontend displays the GUI (Graphic User Interface). All necessary 
 functions and modules are called from here.
-
-Dimensionless representation:
-unit of time        : 1 ms
-unit of inverse time: 10³ /s
-unit of distance    : 100 nm
-unit of Diff.coeff  : 10 µm²/s
-unit of inverse area: 100 /µm²
-unit of inv. volume : 1000 /µm³
-
-Copyright (C) 2011-2012  Paul Müller
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License 
-along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from distutils.version import LooseVersion # For version checking
--- pycorrfit/models/MODEL_TIRF_3D3D.py
+++ pycorrfit/models/MODEL_TIRF_3D3D.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    PyCorrFit
-    This file contains 3D+3D TIR-FCS models.
-
-    Copyright (C) 2011-2012  Paul Müller
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License 
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
+This file contains 3D+3D TIR-FCS models.
 """
 import numpy as np                  # NumPy
 import scipy.special as sps
