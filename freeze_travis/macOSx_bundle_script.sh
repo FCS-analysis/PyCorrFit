5bb25ca9d1c187f643c658871005563a712a2c5e
paulmueller <paul.mueller@biotec.tu-dresden.de>
2015-05-19 18:06:56 +0200

    added -megabytes option for dmg, possible workaround for "hdiutil: create failed - error -5341"
    Former-commit-id: be9878b0434c245fcb00e6430e479d84b1df3457 [formerly 5c43cf01c5dbcf53ba51a7387f9b1f8f16bb97a0] [formerly 0b051236992bd50c6c961558e69f848f07d6fba2 [formerly 293a7e10cfc08e9a6ade8f9ed6e739d151fd6c9a]] [formerly bedc537f08c465d7fdf4cec0d72366f7bacf716e [formerly bb70d15dd72860e13fa8e8ffd847317e3a364829] [formerly adc5fb370aa7288fcbb9a00df93dbcd6a0f5e65a [formerly 85a2ad9d9609c53f749d3426453602a0ec15e2d0]]]
    Former-commit-id: f6392d440081919980a8f014e92e56c2033cf435 [formerly c88b71004aedf407c3bb9dfe8f79a864d8e72b84] [formerly 6363252cce618ddd96bc19640f98c72112a1ce87 [formerly a10a90607bd58327994aa27ed5e4f1e9640ce5c5]]
    Former-commit-id: acb2f9ff44ed1d03459ec573c7d5c65e097775c9 [formerly 850fe5ef11ce48997c3e3e1912f63e42f201309a]
    Former-commit-id: 25093531daf7018ccfbd77c029efcc05a6a7d593

--- freeze_travis/macOSx_bundle_script.sh
+++ freeze_travis/macOSx_bundle_script.sh
@@ -87,8 +87,10 @@ cp ../ChangeLog.txt dmgsrc/
 cp -r ${Progname}".app" dmgsrc/
 # hdiutil: create failed - error -5341
 # http://stackoverflow.com/questions/18621467/error-creating-disk-image-using-hdutil
+# https://discussions.apple.com/thread/4712306
 touch dmgsrc/.Trash
+rm -rf dmgsrc/.DStore
 ls -la dmgsrc/
 # hdiutil create ${DMGname} -srcfolder dmgsrc/ -ov
-hdiutil create -volname "PyCorrFit_image" -format UDZO -imagekey zlib-level=9 -srcfolder dmgsrc/ -ov ${DMGname}
+hdiutil create -volname "PyCorrFit_image" -megabytes 130 -format UDZO -imagekey zlib-level=9 -srcfolder dmgsrc -ov ${DMGname}
 popd
