corrected wrong computation of chi2 which lead to wrong fitting results
Former-commit-id: dd21e2a48a76dcd9262cb71dfc4cf54470a7637e [formerly 911d186dafb177be2ae76e435bdb7fb020073739] [formerly bfabfe9387c889f1708d55f6b9be8fcfd83143cc [formerly b99bf38753e7b88711a4cd5b42b429e75f2669f0]] [formerly 20db52c72108371954738ede6b707faafbf1ae8d [formerly ad4ffc2a304ddf76bcf8e4d55e10198102f46344] [formerly 7e16604b1b6bd77e0bddc5bc3d9590b5cc349723 [formerly aee4af3edad40b7dc05ee8ce7a507d6ffef36f86]]]
Former-commit-id: d0882617dc82f0387b89a7281e17df7189bdaf3c [formerly da91fdd8e12247552449eea20eb89e8ffcfd5a2c] [formerly 6bb42f6865a719724b3b51ae449254e301eaacc1 [formerly 21990ab076d079d488aae03e757562e11501f779]]
Former-commit-id: 57035037d8ee7d293e866be19f0469e99f6e6ae4 [formerly 0501eeb6ad8bf7e2ac792796f919d151a7d2d2b4]
Former-commit-id: c92c6934156f96c36d6f7f544347d3edde46d35e

pycorrfit/fcs_data_set.py
@@ -790,7 +790,7 @@ class Fit(object):
         # Calculate degrees of freedom
         dof = len(self.x) - np.sum(self.fit_bool) - 1
         # This is exactly what is minimized by the scalar minimizers
-        chi2 = self.fit_function_scalar(self.fit_parm, self.x)
+        chi2 = self.fit_function_scalar(self.fit_parm[self.fit_bool], self.x)
         return chi2 / dof
 
     @staticmethod
@@ -1037,6 +1037,7 @@ class Fit(object):
         algorithm = Algorithms[self.fit_algorithm][0]
 
         # Begin fitting
+        
         if self.fit_algorithm == "Lev-Mar":
             res = algorithm(self.fit_function, self.fit_parm[self.fit_bool],
                             args=(self.x,), full_output=1)
@@ -1047,7 +1048,6 @@ class Fit(object):
 
         # The optimal parameters
         parmoptim = res[0]
-
         # Now write the optimal parameters to our values:
         index = 0
         for i in range(len(self.fit_parm)):
@@ -1057,7 +1057,6 @@ class Fit(object):
         # Only allow physically correct parameters
         self.fit_parm = self.check_parms(self.fit_parm)
         # Write optimal parameters back to this class.
-
         chi = self.chi_squared
         # Compute error estimates for fit (Only "Lev-Mar")
         if self.fit_algorithm == "Lev-Mar":
@@ -1080,7 +1079,6 @@ class Fit(object):
                     self.parmoptim_error = np.diag(self.covar)
         else:
             self.parmoptim_error = None
-
 
 
 def GetAlgorithmStringList():