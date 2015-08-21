7a33b458a3ffcd496d2332d95115257b28beb9b0
paulmueller <paul.mueller@biotec.tu-dresden.de>
2015-08-21 13:35:22 +0200

    ALV-7004/USB file format (#104)
    Former-commit-id: 1f60506ebca58dd196fc9648337e7232d07dde47 [formerly 70d54cbadd5afa9ad233ed074c4ea605eee16df4] [formerly e8c5a9bf209eadd9f7749728a70e71c415d50cc7 [formerly 0f0fbde8e00fafe3e6f7b0ac7ad95db54e537ddf]] [formerly 0c90e02fa84e5036611a39c62584330fc4b850e5 [formerly 68dd556d70256d05e0f6c3e7649e8ec0ddd6ce05] [formerly b0f70b193fdb55451797a95550b70116c66891d9 [formerly 07faaffe686eb77b2f54cb6049d0c957d799d8e1]]]
    Former-commit-id: 1c7c0ef753d631b87cc3cd5796d0c864a8defd94 [formerly ed6df4901d2ced8018688de2355198b548a6c681] [formerly c8a7bcd49b93bea6f17690ec53c7aaa6ea5ad8b7 [formerly a3f9f7349d038bc1cb79391923a6911676cf75db]]
    Former-commit-id: 2af1c8f3afbac7c01edc0ecccb23d8346efeeefc [formerly 3d96e51959c43ff6c8695e34bdc4c791bff73644]
    Former-commit-id: 749eb717490bebaf2b989bcc8a305c2bb77bac31

--- pycorrfit/readfiles/read_ASC_ALV.py
+++ pycorrfit/readfiles/read_ASC_ALV.py
@@ -10,54 +10,71 @@ import numpy as np
 
 
 def openASC(dirname, filename):
+    """ 
+    Read data from a ALV .ASC files.
+    """
+    path = os.path.join(dirname, filename)
+    with open(path, 'r') as openfile:
+        Alldata = openfile.readlines()
+    
+    # Open special format?
+    filetype = Alldata[0].strip() 
+    if filetype in ["ALV-7004/USB"]:
+        return openASC_ALV_7004_USB(path)
+    else:
+        # last resort
+        return openASC_old(path)
+
+
+def openASC_old(path):
     """ Read data from a .ASC file, created by
         some ALV-6000 correlator.
 
             ALV-6000/E-WIN Data
-            Date :	"2/20/2012"
+            Date :    "2/20/2012"
             ...
             "Correlation"
-              1.25000E-004	  3.00195E-001
-              2.50000E-004	  1.13065E-001
-              3.75000E-004	  7.60367E-002
-              5.00000E-004	  6.29926E-002
-              6.25000E-004	  5.34678E-002
-              7.50000E-004	  4.11506E-002
-              8.75000E-004	  4.36752E-002
-              1.00000E-003	  4.63146E-002
-              1.12500E-003	  3.78226E-002
+              1.25000E-004      3.00195E-001
+              2.50000E-004      1.13065E-001
+              3.75000E-004      7.60367E-002
+              5.00000E-004      6.29926E-002
+              6.25000E-004      5.34678E-002
+              7.50000E-004      4.11506E-002
+              8.75000E-004      4.36752E-002
+              1.00000E-003      4.63146E-002
+              1.12500E-003      3.78226E-002
             ...
-              3.35544E+004	 -2.05799E-006
-              3.77487E+004	  4.09032E-006
-              4.19430E+004	  4.26295E-006
-              4.61373E+004	  1.40265E-005
-              5.03316E+004	  1.61766E-005
-              5.45259E+004	  2.19541E-005
-              5.87202E+004	  3.26527E-005
-              6.29145E+004	  2.72920E-005
+              3.35544E+004     -2.05799E-006
+              3.77487E+004      4.09032E-006
+              4.19430E+004      4.26295E-006
+              4.61373E+004      1.40265E-005
+              5.03316E+004      1.61766E-005
+              5.45259E+004      2.19541E-005
+              5.87202E+004      3.26527E-005
+              6.29145E+004      2.72920E-005
 
             "Count Rate"
-               1.17188	      26.77194
-               2.34375	      26.85045
-               3.51563	      27.06382
-               4.68750	      26.97932
-               5.85938	      26.73694
-               7.03125	      27.11332
-               8.20313	      26.81376
-               9.37500	      26.82741
-              10.54688	      26.88801
-              11.71875	      27.09710
-              12.89063	      27.13209
-              14.06250	      27.02200
-              15.23438	      26.95287
-              16.40625	      26.75657
-              17.57813	      26.43056
+               1.17188          26.77194
+               2.34375          26.85045
+               3.51563          27.06382
+               4.68750          26.97932
+               5.85938          26.73694
+               7.03125          27.11332
+               8.20313          26.81376
+               9.37500          26.82741
+              10.54688          26.88801
+              11.71875          27.09710
+              12.89063          27.13209
+              14.06250          27.02200
+              15.23438          26.95287
+              16.40625          26.75657
+              17.57813          26.43056
             ...
-             294.14063	      27.22597
-             295.31250	      26.40581
-             296.48438	      26.33497
-             297.65625	      25.96457
-             298.82813	      26.71902
+             294.14063          27.22597
+             295.31250          26.40581
+             296.48438          26.33497
+             297.65625          25.96457
+             298.82813          26.71902
 
         1. We are interested in the "Correlation" section,
         where the first column denotes tau in ms and the second row the
@@ -88,8 +105,9 @@ def openASC(dirname, filename):
          from the file. Elements can be names and must be convertible to
          strings.
     """
-    openfile = open(os.path.join(dirname, filename), 'r')
-    Alldata = openfile.readlines()
+    filename = os.path.basename(path)
+    with open(path, 'r') as openfile:
+        Alldata = openfile.readlines()
     # End of trace
     EndT = Alldata.__len__()
     ## Correlation function
@@ -99,8 +117,6 @@ def openASC(dirname, filename):
             mode = Alldata[i][5:].strip(' ":').strip().strip('"')
             single_strings = ["a-ch0", "a-ch1", "auto ch0", "auto ch1",
                               "fast auto ch0", "fast auto ch1",
-                              "a-ch0+1  c-ch0/1+1/0",
-                              "a-ch1+0  c-ch0/1+0/1",
                                ]
             if (mode.lower().count('single') or
                 mode.lower().strip() in single_strings):
@@ -130,7 +146,7 @@ def openASC(dirname, filename):
             # There are several curves now.
             StartC = i+2
         if Alldata[i].replace(" ", "").lower().strip() == '"countrate"':
-            # takes cate of "Count Rate" and "Countrate"
+            # takes care of "Count Rate" and "Countrate"
             # End of correlation function
             EndC = i-1
             # Start of trace (goes until end of file)
@@ -194,8 +210,6 @@ def openASC(dirname, filename):
             for i in np.arange(len(curvelist)-1):
                 trace2.append(list())
                 trace2[i+1].append((np.float(row[0])*timefactor, 0))
-    # return as an array
-    openfile.close()
 
     # group the resulting curves
     corrlist = list()
@@ -317,6 +331,116 @@ def openASC(dirname, filename):
     filelist = list()
     for i in curvelist:
         filelist.append(filename)
+    dictionary["Filename"] = filelist
+    
+    return dictionary
+
+
+def openASC_ALV_7004_USB(path):
+    """
+    Opens ALV file format with header information "ALV-7004/USB" 
+    
+    This is a single-run file format.
+    - data is identified by 4*"\t"
+    - count rate is identified by string (also "countrate")
+    - allzero-correlations are removed
+
+    "Correlation"
+      2.50000E-005     -9.45478E-001     -1.00000E+000      5.22761E-002      3.05477E-002
+      5.00000E-005      6.73734E-001     -2.59938E-001      3.17894E-002      4.24466E-002
+      7.50000E-005      5.30716E-001      3.21605E-001      5.91051E-002      2.93061E-002
+      1.00000E-004      3.33292E-001      1.97860E-001      3.24102E-002      3.32379E-002
+      1.25000E-004      2.42538E-001      1.19988E-001      4.37917E-002      3.05477E-002
+      1.50000E-004      1.86396E-001      1.23318E-001      5.66218E-002      2.25806E-002
+      1.75000E-004      1.73836E-001      8.53991E-002      4.64819E-002      3.46865E-002
+      2.00000E-004      1.48080E-001      9.35377E-002      4.37917E-002      4.17223E-002
+    [...]
+      1.00663E+004      2.80967E-005     -2.23975E-005     -7.08272E-005      5.70470E-005
+      1.09052E+004      9.40185E-005      2.76261E-004      1.29745E-004      2.39958E-004
+      1.17441E+004     -2.82103E-004     -1.97386E-004     -2.88753E-004     -2.60987E-004
+      1.25829E+004      1.42069E-004      3.82018E-004      6.03932E-005      5.40363E-004
+    
+    "Count Rate"
+           0.11719         141.83165          81.54211         141.83165          81.54211
+           0.23438         133.70215          77.90344         133.70215          77.90344
+           0.35156         129.67148          74.58858         129.67148          74.58858
+           0.46875         134.57133          79.53957         134.57133          79.53957
+    [...]
+      29.29688         143.78307          79.06236         143.78307          79.06236
+      29.41406         154.80135          82.87147         154.80135          82.87147
+      29.53125         187.43013          89.61197         187.43013          89.61197
+      29.64844         137.82655          77.71597         137.82655          77.71597
+    [...]
+
+
+    """
+    filename = os.path.basename(path)
+    with open(path, 'r') as openfile:
+        Alldata = openfile.readlines()
+    
+    # Find the different arrays
+    # correlation array: "  "
+    # trace array: "       "
+    Allcorr = list()
+    Alltrac = list()
+    i=0
+    intrace = False
+    for item in Alldata:
+        if item.lower().strip().strip('"').replace(" ", "") == "countrate":
+            intrace = True
+        i += 1
+        if item.count("\t") == 4: 
+            if intrace:
+                it = item.split("\t")
+                it = [ float(t.strip()) for t in it ]
+                Alltrac.append(it)
+            else:
+                ic = item.split("\t")
+                ic = [ float(c.strip()) for c in ic ]
+                Allcorr.append(ic)
+    Allcorr = np.array(Allcorr)
+    Alltrac = np.array(Alltrac)
+    
+    # Allcorr: lag time, ac1, ac2, cc12, cc21
+    # Alltrac: time, trace1, trace2, trace1, trace2
+    assert np.allclose(Alltrac[:,1], Alltrac[:,3], rtol=.01), "unknown ALV file format"
+    assert np.allclose(Alltrac[:,2], Alltrac[:,4], rtol=.01), "unknown ALV file format"
+    
+    guesstypelist = ["AC1", "AC2", "CC12", "CC21"]
+    typelist = list()
+    corrlist = list()
+    tracelist = list()
+    filelist = list()
+    
+    lagtime = Allcorr[:,0]
+    time = Alltrac[:,0]*1000
+    trace1 = np.dstack((time, Alltrac[:,1]))[0]
+    trace2 = np.dstack((time, Alltrac[:,2]))[0]
+    
+    for i, typ in enumerate(guesstypelist):
+        corr = np.dstack((lagtime, Allcorr[:,i+1]))[0]
+        
+        if not np.allclose(corr[:,1], np.zeros_like(lagtime)):
+            # type
+            typelist.append(typ)
+            # correlation
+            corrlist.append(corr)
+            # trace
+            if typ.count("CC"):
+                tracelist.append([trace1, trace2])
+            elif typ.count("AC1"):
+                tracelist.append([trace1])
+            elif typ.count("AC2"):
+                tracelist.append([trace2])
+            else:
+                raise ValueError("Unknown ALV file format")
+            # filename
+            filelist.append(filename)
+
+    dictionary = dict()
+    dictionary["Correlation"] = corrlist
+    dictionary["Trace"] = tracelist
+    dictionary["Type"] = typelist
     dictionary["Filename"] = filelist
     return dictionary
 
