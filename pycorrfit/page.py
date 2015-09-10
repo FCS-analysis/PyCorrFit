5dae40dfdbdec412f194843a5791b16bc354304c
paulmueller <paul.mueller@biotec.tu-dresden.de>
2015-09-10 08:55:08 +0200

    improve charcter display (#114)

--- pycorrfit/page.py
+++ pycorrfit/page.py
@@ -22,6 +22,43 @@ from . import fcs_data_set as pcfbase
 from .fcs_data_set import Correlation, Fit
 
 
+def float2string_nsf(fval, n=7):
+    """
+    Truncate a float to n significant figures and return nice string.
+    Arguments:
+      q : a float
+      n : desired number of significant figures
+    Returns:
+    String with only n s.f. and trailing zeros.
+    """
+    #sgn=np.sign(fval)
+    if fval == 0:
+        npoint=n
+    else:
+        q=abs(fval)
+        k=int(np.ceil(np.log10(q/n)))
+        npoint = n-k
+    string="{:.{}f}".format(fval, npoint)
+    return string
+
+def nice_string(string):
+    """
+    Convert a string of a float created by `float2string_nsf`
+    to something nicer.
+    
+    i.e.
+    - 1.000000 -> 1
+    - 1.010000 -> 1.010
+    """
+    if len(string.split(".")[1].replace("0", "")) == 0:
+        return "{:d}".format(int(float(string)))
+    else:
+        olen = len(string)
+        newstring = string.rstrip("0")
+        if olen > len(newstring):
+            string=newstring+"0"
+        return string
+
 class PCFFloatValidator(wx.PyValidator):
     def __init__(self, flag=None, pyVar=None):
         wx.PyValidator.__init__(self)
@@ -41,23 +78,42 @@ class PCFFloatValidator(wx.PyValidator):
 
         return True
 
-
     def OnChar(self, event):
+        """
+        Filter the characters that are put in the control.
+        
+        TODO:
+        - check for strings that do not make sense
+          - 2e-4.4
+          - 2e--3
+          - 3-1+5
+        """
         key = event.GetKeyCode()
+        ctrl = event.GetEventObject()
+        # Get the actual string from the object
+        curval = wx.TextCtrl.GetValue(ctrl)
 
         if key < wx.WXK_SPACE or key == wx.WXK_DELETE or key > 255:
             event.Skip()
             return
 
-        if ( chr(key) in string.digits or
-             chr(key) in ["+", "-", ".", ",", "e"]):
+        char = chr(key)
+        char = char.replace(",", ".")
+        
+        onlyonce = [".", "e"]
+        if char in onlyonce and curval.count(char):
+            # not allowed
+            return
+
+        if ( char in string.digits or
+             char in ["+", "-", ".", "e"]):
             event.Skip()
             return
 
         if not wx.Validator_IsSilent():
             wx.Bell()
 
-        # Returning without calling even.Skip eats the event before it
+        # Returning without calling event.Skip eats the event before it
         # gets to the text control
         return
 
@@ -68,6 +124,7 @@ class PCFFloatTextCtrl(wx.TextCtrl):
                              style=wx.TE_PROCESS_ENTER, **kwargs)
         self.Bind(wx.EVT_ENTER_WINDOW, self.OnMouseEnter)
         self.Bind(wx.EVT_LEAVE_WINDOW, self.OnMouseLeave)
+        self._PCFvalue = 0.0
 
     def OnMouseEnter(self, e):
         self.SetFocus()
@@ -78,13 +135,30 @@ class PCFFloatTextCtrl(wx.TextCtrl):
         self.SetInsertionPoint(0)
 
     def SetValue(self, value):
-        value = str(value)
-        wx.TextCtrl.SetValue(self, value)
+        self._PCFvalue = value
+        string = PCFFloatTextCtrl.float2string(value)
+        wx.TextCtrl.SetValue(self, string)
     
     def GetValue(self):
         string = wx.TextCtrl.GetValue(self)
-        return(PCFFloatTextCtrl.string2float(string))
-    
+        if string == PCFFloatTextCtrl.float2string(self._PCFvalue):
+            # use internal value: more accurate
+            #print("internal", self._PCFvalue)
+            return self._PCFvalue
+        else:
+            # new value
+            #print("external", string)
+            return PCFFloatTextCtrl.string2float(string)
+        
+    @staticmethod
+    def float2string(value):
+        """
+        inverse of string2float with some tweaks
+        """
+        value = float2string_nsf(value)
+        value = nice_string(value)
+        return value
+        
     @staticmethod
     def string2float(string):
         """
@@ -96,17 +170,14 @@ class PCFFloatTextCtrl(wx.TextCtrl):
         string = string.replace(",", ".")
         # allow only one decimal point
         string = string[::-1].replace(".", "", string.count(".")-1)[::-1]
-        # see if we have a string like "2e-4"
         try:
             string = "{:.12f}".format(float(string))
         except:
             pass
         # remove letters
-        try:
-            string = re.sub(r'[^\d.-]+', '', string)
-        except:
-            import IPython
-            IPython.embed()
+        string = re.sub(r'[^\d.-]+', '', string)
+        if len(string) == 0:
+            string = "0"
         return float(string)
 
 
