0768116616cc5f91a01d8615656b9e0a6efd406c
paulmueller <paul.mueller@biotec.tu-dresden.de>
2017-03-02 19:35:25 +0100

    fix docker

--- ci_linux/build-wheels.sh
+++ ci_linux/build-wheels.sh
@@ -3,14 +3,14 @@ set -e -x
 
 # Test 
 for PYBIN in /opt/python/cp27*/bin/; do
+    "${PYBIN}/pip" install --upgrade pip
+    "${PYBIN}/pip" install -e /io
     "${PYBIN}/python" /io/setup.py develop
     "${PYBIN}/python" /io/setup.py test
-    "${PYBIN}/python" /io/setup.py clean
 done
 
 # Compile wheels
 for PYBIN in /opt/python/cp27*/bin; do
-    "${PYBIN}/pip" install -e /io
     "${PYBIN}/python" /io/setup.py develop
     "${PYBIN}/pip" wheel /io/ -w dist/
 done
