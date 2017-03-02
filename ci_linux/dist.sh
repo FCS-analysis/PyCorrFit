7587d954ae7dbea8626804e88e080f7472e4bc60
paulmueller <paul.mueller@biotec.tu-dresden.de>
2017-03-03 00:49:00 +0100

    fix basename

--- ci_linux/dist.sh
+++ ci_linux/dist.sh
@@ -4,7 +4,7 @@
 set -e -v
 # remove all .pyc files to prevent error:
 # - import file mismatch / unique basename
-find . -name \*.pyc -delete
+find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 docker pull $DOCKER_IMAGE
 mkdir -p dist
 docker run -e TRAVIS_PYTHON_VERSION --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/ci_linux/build-wheels.sh
