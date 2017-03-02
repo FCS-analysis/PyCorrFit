64deaa7d97061eacf3fd43dbba528eb995699965
paulmueller <paul.mueller@biotec.tu-dresden.de>
2017-03-03 00:40:16 +0100

    fix basename

--- ci_linux/dist.sh
+++ ci_linux/dist.sh
@@ -2,6 +2,9 @@
 # Call this script from the parent directory
 # This will produce wheels using docker
 set -e -v
+# remove all .pyc files to prevent error:
+# - import file mismatch / unique basename
+find . -name \*.pyc -delete
 docker pull $DOCKER_IMAGE
 mkdir -p dist
 docker run -e TRAVIS_PYTHON_VERSION --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/ci_linux/build-wheels.sh
