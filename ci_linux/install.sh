456ee15e8dffa3a72201aa4b45d571bc3fdc4fd8
paulmueller <paul.mueller@biotec.tu-dresden.de>
2017-03-02 15:30:44 +0100

    fix travis

--- ci_linux/install.sh
+++ ci_linux/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get -y update
-sudo apt-get -y -qq install cython python-matplotlib python-numpy python-pip python-scipy python-sympy python-wxgtk2.8 virtualenv --system-site-packages ~/env
+sudo apt-get -y -qq install cython python-matplotlib python-numpy python-pip python-scipy python-sympy python-wxgtk2.8 virtualenv
+virtualenv --system-site-packages ~/env
 source ~/env/bin/activate
