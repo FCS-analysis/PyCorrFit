aaf8198fe1e284b1758568535ce88ebf766fe59c
paulmueller <paul.mueller@biotec.tu-dresden.de>
2017-03-02 16:34:49 +0100

    skip gui tests

--- ci_linux/install.sh
+++ ci_linux/install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 deactivate
-curl http://apt.wxwidgets.org/key.asc | sudo apt-key add - 
 sudo apt-get update -qq
-sudo apt-get install -qq python-pip python-wxgtk2.8
+sudo apt-get install -qq python-pip
 sudo pip install virtualenv
 # use separate virtual environment
 virtualenv --system-site-packages ~/env
 source ~/env/bin/activate
 pip install --upgrade pip
-pip install scipy sympy numpy matplotlib cython
+pip install cython matplotlib lmfit numpy scipy sympy
 
