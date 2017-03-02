4a12f26801eae61ed7729b406d3c318ef6761214
paulmueller <paul.mueller@biotec.tu-dresden.de>
2017-03-02 13:32:23 +0100

    fix travis

--- ci_linux/install.sh
+++ ci_linux/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# remove any previously installed version of numpy
 pip uninstall numpy
-sudo apt-get -qy uninstall python-numpy
+sudo apt-get -qy remove python-numpy
+# install pcf dependencies
 sudo apt-get -qy install  python-wxgtk2.8 python-numpy python-scipy python-pip cython python-matplotlib python-sympy
 
