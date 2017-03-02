ac3e5be46d56361960c13fe5d7f8fd91f3265045
paulmueller <paul.mueller@biotec.tu-dresden.de>
2017-03-03 00:29:53 +0100

    fix audit

--- ci_linux/build-wheels.sh
+++ ci_linux/build-wheels.sh
@@ -5,12 +5,12 @@ cd /io
 
 for PYBIN in /opt/python/cp${TRAVIS_PYTHON_VERSION//./}*/bin/; do
     # Install
-    "${PYBIN}/pip" install -e . --ignore-installed
+    "${PYBIN}/pip" install -e .
     "${PYBIN}/python" setup.py build_ext --inplace --force
     # Test
     "${PYBIN}/python" setup.py test
     # Wheels
-    "${PYBIN}/pip" wheel . -w dist/
+    "${PYBIN}/pip" wheel /io/ -w dist/ --no-deps
     rm -rf .eggs
 done
 
