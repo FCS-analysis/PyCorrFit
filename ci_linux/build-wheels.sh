8f5988f78d8798836a81ffc0c3f815b2fc1549f6
paulmueller <paul.mueller@biotec.tu-dresden.de>
2017-03-02 19:51:11 +0100

    fix docker

--- ci_linux/build-wheels.sh
+++ ci_linux/build-wheels.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -e -x
 
+cd /io
+
 # Test 
 for PYBIN in /opt/python/cp27*/bin/; do
     "${PYBIN}/pip" install --upgrade pip
-    "${PYBIN}/pip" install -e /io
-    "${PYBIN}/python" /io/setup.py develop
-    "${PYBIN}/python" /io/setup.py test
+    "${PYBIN}/pip" install -e .
+    "${PYBIN}/python" setup.py develop
+    "${PYBIN}/python" setup.py test
 done
 
 # Compile wheels
 for PYBIN in /opt/python/cp27*/bin; do
-    "${PYBIN}/python" /io/setup.py develop
-    "${PYBIN}/pip" wheel /io/ -w dist/
+    "${PYBIN}/python" setup.py develop
+    "${PYBIN}/pip" wheel . -w dist/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in dist/*.whl; do
+for whl in /io/dist/*.whl; do
     auditwheel repair "$whl" -w /io/dist/
 done
 
