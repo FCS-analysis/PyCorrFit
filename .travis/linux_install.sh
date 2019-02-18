6b5dbb3c3b5b1a34013136dccc80d4fe385cbfe9
Paul Müller <dev@craban.de>
2019-02-18 23:47:28 +0100

    ci: omit python version

--- .travis/linux_install.sh
+++ .travis/linux_install.sh
@@ -9,7 +9,7 @@ sudo apt-get install -qq python3-pip python3-tk virtualenv
 # see https://github.com/wxWidgets/Phoenix/blob/master/README.rst
 #sudo apt-get install -qq libgtk2.0 libgtk2.0-dev libwebkitgtk-dev dpkg-dev build-essential python3.6-dev libjpeg-dev libtiff-dev libsdl1.2-dev libnotify-dev freeglut3 freeglut3-dev libsm-dev libgtk-3-dev libwebkit2gtk-4.0-dev libxtst-dev libgstreamer-plugins-base1.0-dev
 # use separate virtual environment
-virtualenv --system-site-packages -p python3.6 ~/env
+virtualenv --system-site-packages ~/env
 source ~/env/bin/activate
 pip install --upgrade pip
 pip install cython matplotlib lmfit numpy scipy sympy
