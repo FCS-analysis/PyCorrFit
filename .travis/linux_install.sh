3e76cbaaf8ed40bb1954824e251fe5807fbbd620
Paul Müller <dev@craban.de>
2019-02-18 23:44:43 +0100

    ci: force python3.6

--- .travis/linux_install.sh
+++ .travis/linux_install.sh
@@ -4,14 +4,12 @@ deactivate
 sudo apt-get update -qq
 # Fixes the error:
 # - ImportError: No module named _tkinter, please install the python-tk package
-sudo apt-get install -qq python3-pip python3-tk
+sudo apt-get install -qq python3-pip python3-tk virtualenv
 # requirements for building wxPython4 on Ubuntu 17.10
 # see https://github.com/wxWidgets/Phoenix/blob/master/README.rst
 #sudo apt-get install -qq libgtk2.0 libgtk2.0-dev libwebkitgtk-dev dpkg-dev build-essential python3.6-dev libjpeg-dev libtiff-dev libsdl1.2-dev libnotify-dev freeglut3 freeglut3-dev libsm-dev libgtk-3-dev libwebkit2gtk-4.0-dev libxtst-dev libgstreamer-plugins-base1.0-dev
-# install virtualenv via pip as it is not in the repositories for trusty
-sudo pip3 install virtualenv
 # use separate virtual environment
-virtualenv --system-site-packages -p python3 ~/env
+virtualenv --system-site-packages -p python3.6 ~/env
 source ~/env/bin/activate
 pip install --upgrade pip
 pip install cython matplotlib lmfit numpy scipy sympy
